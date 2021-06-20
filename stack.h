044976138f9019a99d6cb53fd9f3d2727c13ee0c
oxtna <rafalkajomof2709@gmail.com>
2021-06-20 14:27:06 +0200

    Documented stack operations

--- stack.h
+++ stack.h
@@ -7,8 +7,17 @@
 
 typedef struct _stack stack;
 
+// Create a stack allocated on the heap and initialize its values.
 stack *s_create();
+
+// Push a new value onto the stack. Returns ERROR on failure and SUCCESS on success.
 int s_push(stack *s, int value);
+
+// Pop a value off the stack. Returns ERROR on failure and the value on success.
 int s_pop(stack *s);
+
+// Peek at the value on top of the stack. Returns ERROR on failure and the value on success.
 int s_peek(stack *s);
+
+// Returns true if the stack is empty, false if it is not, and BOOL_ERROR on failure.
 bool s_is_empty(stack *s);
